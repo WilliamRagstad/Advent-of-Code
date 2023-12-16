d110a20a6e6216376a06fa7faf991a76a0c31a77
William Rågstad <william.ragstad@gmail.com>
2023-12-16 21:22:33 +0100

    Update template

--- 2023/day-tmpl/solve.fsx
+++ 2023/day-tmpl/solve.fsx
@@ -1,12 +1,22 @@
 open System
-let input = System.IO.File.ReadAllLines("input.txt")
+
+let format (s: string) = s.Trim().Replace("\r", "")
+
+let lines (s: string) =
+    (format s).Split('\n') |> Array.map (fun l -> l.Trim())
+
+let input = System.IO.File.ReadAllText("input.txt") |> format
 
 let debug value =
     printfn "%A" value
     value
 
-let lines (s: string) =
-    s.Trim().Split('\n') |> Array.map (fun l -> l.Trim())
+let example_assert (part: int) (example: int) (expected: 'a) (value: 'a) =
+    if value = expected then
+        printfn "P%d example %d: %A (correct)" part example value
+    else
+        printfn "P%d example %d: Expected %A, got %A" part example expected value
+        Environment.Exit(1)
 
 // --- Part One ---
 (**
@@ -21,7 +31,8 @@ let example_2 = @"" |> lines
 let example_3 = @"" |> lines
 
 let solve_p1 a = ()
-let p1 = input |> solve_p1
+example_assert 1 1 () (example_1 |> solve_p1)
+let p1 = input |> lines |> solve_p1
 printfn "P1: %A" p1
 // Tries:
 
@@ -35,8 +46,8 @@ Conclusion:
 **)
 
 let solve_p2 a = ()
-
-let p2 = input |> solve_p2
+example_assert 2 1 () (example_1 |> solve_p2)
+let p2 = input |> lines |> solve_p2
 printfn "P2: %A" p2
 // Tries:
 // 87263515 - correct
