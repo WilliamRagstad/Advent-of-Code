53e9f5abeeca12b05c5950e7ffdff3175f1ec01e
WilliamRagstad <william.ragstad@gmail.com>
2024-12-04 20:32:04 +0100

    Add template

---
+++ 2024/template/solve.hs
@@ -0,0 +1,13 @@
+main = interact $ show . (\input -> (solve1 input, solve2 input))
+
+---------------- SHARED ----------------
+
+---------------- SOLUTION 1 ----------------
+
+solve1 :: String -> Int
+solve1 input = -1
+
+---------------- SOLUTION 2 ----------------
+
+solve2 :: String -> Int
+solve2 input = -1
